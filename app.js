7eeaf8fa21b74a6db5fc3deaa1cb1e19cfd9c989
Neta Hadar Itzhak <netook6@gmail.com>
2021-11-28 13:12:46 +0200

    Refactor: fix according to eslint and add Helmet

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
 const cards = require('./routes/cards');
+
 const app = express();
 const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
+app.use(helmet());
 app.use('/', users);
 app.use('/', cards);
 
 app.get('*', (req, res) => {
-  res.send(404, {message: 'Page Not Found'})
-})
+  res.send(404, { message: 'Page Not Found' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-})
+});
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,31 @@
 const users = require('express').Router();
 const fsPromises = require('fs').promises;
 const path = require('path');
+
 const usersFilePath = path.join(__dirname, '../data/users.json');
 
-const readUsersFile =   fsPromises.readFile(usersFilePath, {encoding: 'utf-8'});
+const readUsersFile = fsPromises.readFile(usersFilePath, { encoding: 'utf-8' });
 
-//Get full users list
+// Get full users list
 users.get('/users', (req, res) => {
   readUsersFile
-  .then((usersData)=> { res.send(200, JSON.parse(usersData));})
-  .catch(() => res.send(500, { message: 'An error has occurred' }))
+    .then((usersData) => { res.send(200, JSON.parse(usersData)); })
+    .catch(() => res.send(500, { message: 'An error has occurred' }));
 });
 
-//Get user by id
+// Get user by id
 users.get('/users/:id', (req, res) => {
   readUsersFile
-  .then((usersData)=> {
-    usersData = JSON.parse(usersData);
-    const user = usersData.find((user) => user._id === req.params.id);
-    if(!user){
-      res.send(404, { message: 'User not found' });
-      return;
-    }else{
-    res.send(200, user)}
-  })
-  .catch(() => res.send(500, { message: 'An error has occurred' }))
+    .then((usersData) => {
+      const parsedUsersData = JSON.parse(usersData);
+      const chosenUser = parsedUsersData.find((user) => user._id === req.params.id);
+      if (!chosenUser) {
+        res.send(404, { message: 'User not found' });
+      } else {
+        res.send(200, chosenUser);
+      }
+    })
+    .catch(() => res.send(500, { message: 'An error has occurred' }));
 });
 
 module.exports = users;
